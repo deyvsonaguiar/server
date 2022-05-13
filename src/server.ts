9242a0bfa4eb920827307309113dd4152a2a438d
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2022-05-13 14:32:30 -0400

    Testando envio de email de feedback criado com mailtrap

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express'
 import { prisma } from './prisma'
+import nodemailer from 'nodemailer'
 
 const app = express()
-
 app.use(express.json())
+
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "a03e73388c8f59",
+      pass: "1c3031709a1852"
+    }
+  })
 
 app.post('/feedbacks',async (req, res) => {
     const { type, comment, screenshot } = req.body
@@ -14,6 +24,18 @@ app.post('/feedbacks',async (req, res) => {
             comment, 
             screenshot
         }
+    })
+
+    transport.sendMail({
+        from: 'Equipe Feedget <atendimento@feedget.com',
+        to: 'Deyvson Aguiar <deyvsonaguiar@gmail.com',
+        subject: 'Novo feedback',
+        html: [
+            `<div style="font-family: sans-serif; font-size: 16px; color: #111">`,
+            `<p>Tipo de comentário: ${type}</p>`,
+            `<p>Comentário: ${comment}</p>`,
+            `</div>`,
+        ].join('\n')
     })
 
     return res.status(201).json({ data: feedback })
