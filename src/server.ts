3fb1f2ec79779f423d8afbe76221fd49ac1409c3
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2022-05-12 23:56:43 -0400

    Configurando o Express e o Prisma ORM

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
-const server = 1;
+import express from 'express'
 
-async function teste() {
-    return 'ok';
-}
+const app = express()
+
+app.get('/users', (req, res) => {
+    return res.send('Hello NLW Return - Node Server')
+})
+
+app.listen(3333, () => {
+    console.log("HTTP Server running!")
+})
